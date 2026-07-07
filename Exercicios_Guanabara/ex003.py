293408a80088d0712ac165014b676ba05574eb43
Joseg-silva <josegabriel152024@gmail.com>
2026-07-07 17:13:52 -0300

    altracão teste

--- Exercicios_Guanabara/ex003.py
+++ Exercicios_Guanabara/ex003.py
@@ -3,3 +3,4 @@ valor_2 = int(input('Digite outro numero: '))
 resultado = valor_1 + valor_2 
 print(f'A soma entre {valor_1} e {valor_2} e igual a {resultado}')
 #print(f'A soma entre {valor_1} e {valor_2} e igual a {valor_1 + valor_2}')
+
