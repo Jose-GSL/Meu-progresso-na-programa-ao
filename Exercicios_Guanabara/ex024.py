389409f5f6b986c0f6bad125ddd89aac985f29fc
Joseg-silva <josegabriel152024@gmail.com>
2026-07-09 11:54:54 -0300

    Exercio 24- arrumando
    agora ele ver se o nome da cidade começa com santos

--- Exercicios_Guanabara/ex024.py
+++ Exercicios_Guanabara/ex024.py
@@ -1,3 +1,4 @@
 #ver se sua cidade começa com o nome santos.
 cidade = str(input('Digite o nome da sua cidade: ')).strip()
-print('Santos' in  cidade.capitalize())
+separador = cidade.split()
+print('Santos' in  separador[0].capitalize())
