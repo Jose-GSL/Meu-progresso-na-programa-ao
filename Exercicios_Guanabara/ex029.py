613389d9849ba2f1d2c369d1616c47d3fee56681
Joseg-silva <josegabriel152024@gmail.com>
2026-08-24 08:53:08 -0300

    continuaçao do curso ex 29
    Limite de velocidade usando a condição if, e dando uma multa caso exceda o limite de velocidade

--- Exercicios_Guanabara/ex029.py
+++ Exercicios_Guanabara/ex029.py
@@ -1,10 +1,19 @@
-velocidade = int(input('Qual e a velocidade atual do carro? '))
+#Forma que eu fiz
+velocidade = float(input('Qual e a velocidade atual do carro? '))
 permitido = 80
-if velocidade == permitido:
-    print('Tenha um bom dia! Dirija com segurança!')
-else:
-    multa = (velocidade - 80) * 7 
-    print(f'O limite de velocidade e {permitido}km/h ')
-    print('Você levarar uma multa de R${multa}')
+if velocidade > permitido:
+    multa = (velocidade - permitido) * 7 
+    print(f'Multado!Você  lexeceu o limite de velocidade que de {permitido}km/h ')
+    print(f'Você levarar uma multa de R${multa:.2f}')
+print('Tenha um bom dia! Dirija com segurança!')
 
 
+#Forma do curso
+'''
+velocidade = float(input('Qual é a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO!Você excedeu o limite de velocidade  que é de 80km/h')
+    multa = (velocidade - 80) * 7
+    print(f'Você deve pagar uma multa de R${multa:.2f}')
+print('Tenha um bom dia! Dirija com cuidado!')
+'''
