23d7677f2431004c600f14c42b6abd235b5239fa
Joseg-silva <josegabriel152024@gmail.com>
2026-08-24 09:10:21 -0300

    ex 30
    digite um numero e o programara mostrara se e par ou impar, atraves de uma formula matecatica usando o modulo que pega o resto da divisão

---
+++ Exercicios_Guanabara/ex30.py
@@ -0,0 +1,9 @@
+#Forma que fiz 
+'''Par ou impar'''
+
+numero = int(input('Digite um numero qualquer: '))
+
+if numero % 2  == 0:
+    print(f'O numero {numero} é PAR')
+else:
+    print(f'O numero {numero} é IMPAR')
